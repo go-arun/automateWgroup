d771dafa4fee33b8bd22d191e4d022a5e12f4c2a
Arun <kcarun@yahoo.com>
2020-09-20 08:08:03 +0530

    uploading files first step over

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/go-arun/fishrider/modules/session"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"log"
+	"io"
+
 )
 
 func adminGet( c *gin.Context){
@@ -31,12 +35,43 @@ func adminPost( c *gin.Context){
 		gin.H{"title": "Admin Panel"},
 	)
 }
+func adminPanelPost(c *gin.Context){
+	file, header, err := c.Request.FormFile("file") 
+	if err != nil {
+	  c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+	  return
+	}
+	filename := header.Filename
+	out, err := os.Create("public/" + filename)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+	  log.Fatal(err)
+	}
+ 
+}
+
+
+
+
+
+const (
+	SR_File_Max_Bytes = 1024 * 1024 * 2
+)
+
 func main(){
 	db.Connect() //db Connection 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/admin", adminGet)
 	router.POST("/admin", adminPost)
+	router.POST("/admin_panel", adminPanelPost)
+	router.StaticFS("/file", http.Dir("public"))
+
+
 	router.Run()
 }
 
