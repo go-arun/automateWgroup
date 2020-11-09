d7796458ddb50f72fa695039c56ef112a8647719
Arun <kcarun@yahoo.com>
2020-11-09 18:26:13 +0530

    Modificed the script in a way that, it will build , then move the binary to the location where the bianry then it will restart ther service too

--- Scripts/buidSource.sh
+++ Scripts/buidSource.sh
@@ -1,2 +1,4 @@
 /usr/bin/echo "Success" >> /tmp/scriptWorked
-sudo /usr/local/go/bin/go build main.go
+sudo /usr/local/go/bin/go build ../main.go
+sudo /usr/bin/mv -f main /home/ubuntu/automateWgroup
+sudo /usr/sbin/service goweb restart
