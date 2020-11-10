7de6dc056bca6eb51b9764ec54a1022f4e9dbc2e
Arun <kcarun@yahoo.com>
2020-11-10 13:18:04 +0530

    mvoing build resut part is removed to check whether it is generation build output

--- Scripts/buidSource.sh
+++ Scripts/buidSource.sh
@@ -1,4 +1,3 @@
 /usr/bin/echo "Success" >> /tmp/scriptWorked
 /usr/local/go/bin/go build ../main.go
-/usr/bin/mv -f /var/www/html/WordPress/Scripts/main /home/ubuntu/automateWgroup
 /usr/sbin/service goweb restart
