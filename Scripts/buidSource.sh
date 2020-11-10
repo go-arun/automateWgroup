b62f0daba6c0a854e8e70217c1781df0d433a4cf
Arun <kcarun@yahoo.com>
2020-11-10 14:09:25 +0530

    bash heading added in file

--- Scripts/buidSource.sh
+++ Scripts/buidSource.sh
@@ -1,3 +1,4 @@
-/usr/bin/echo "Success" >> /tmp/scriptWorked
+#!/bin/bash
+/usr/bin/echo "Success-" >> /tmp/scriptWorked
 /usr/local/go/bin/go build -o /home/ubuntu/automateWgroup/main /var/www/html/WordPress/main.go
 /usr/sbin/service goweb restart
