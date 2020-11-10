bf3fe3a99d6649f196a4659efc10c61750c7f3b3
Arun <kcarun@yahoo.com>
2020-11-10 14:56:54 +0530

    add sleep in script

--- Scripts/buidSource.sh
+++ Scripts/buidSource.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /usr/bin/echo "Success-" >> /tmp/scriptWorked
 /usr/local/go/bin/go build -o /home/ubuntu/automateWgroup/main /var/www/html/WordPress/main.go
+sleep 10
 /usr/sbin/service goweb restart
